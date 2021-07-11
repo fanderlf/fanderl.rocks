cbb2071ab81ec0097dccf570a2a59ba0ad64e532
Florian Fanderl <florian.fanderl@googlemail.com>
2021-07-11 20:46:53 +0200

    fixed nginx setup and updated the page content to reflect changes

--- run_local_in_docker.sh
+++ run_local_in_docker.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# docker container stop nginx_fanderl_rocks
+# docker container rm nginx_fanderl_rocks
+# docker build -t nginx_fanderl_rocks .
+# docker run --name nginx_fanderl_rocks -p 80:80 -d nginx_fanderl_rocks
+
 docker container stop nginx_fanderl_rocks
 docker container rm nginx_fanderl_rocks
-docker run --name nginx_fanderl_rocks --mount type=bind,source="$(pwd)"/content,target=/usr/share/nginx/html,readonly --mount type=bind,source="$(pwd)"/nginx_conf,target=/etc/nginx/conf,readonly -p 80:80 -d nginx
+docker run --name nginx_fanderl_rocks --mount type=bind,source="$(pwd)"/content,target=/usr/share/nginx/html,readonly -p 80:80 -d nginx
---
+++ shell_in_docker.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker exec -it nginx_fanderl_rocks /bin/bash
