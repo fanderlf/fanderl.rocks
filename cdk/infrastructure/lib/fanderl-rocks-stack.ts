7b6e340086f4a348d8bd3b217efdd053473bb77f
Florian Fanderl <florian.fanderl@googlemail.com>
2022-02-07 13:22:15 +0100

    added server side logs for fanderl.rocks

--- cdk/infrastructure/lib/fanderl-rocks-stack.ts
+++ cdk/infrastructure/lib/fanderl-rocks-stack.ts
@@ -9,12 +9,20 @@ export class FanderlRocksStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const root_logs_bucket = new s3.Bucket(this, 'fanderl_rocks_logs_bucket', {
+      versioned: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      bucketName: 'fanderl-rocks-logs'
+    })
+
     const root_bucket = new s3.Bucket(this, 'fanderl_rocks_domain_root_bucket', {
       versioned: false,
       bucketName: 'fanderl.rocks',
       websiteIndexDocument: 'index.html',
       publicReadAccess: true,
-      removalPolicy: RemovalPolicy.DESTROY
+      removalPolicy: RemovalPolicy.DESTROY,
+      serverAccessLogsBucket: root_logs_bucket,
+      serverAccessLogsPrefix: 'fanderl_rocks_root'
     })
 
     new s3Deployment.BucketDeployment(this, 'fanderl_rocks_static_website_content', {
