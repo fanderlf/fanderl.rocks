c1d229e1370368beca95d5ed1c87011022fe02a3
Florian Fanderl <florian.fanderl@googlemail.com>
2022-02-09 20:16:32 +0100

    moved to cloudfront + https

--- cdk/infrastructure/lib/fanderl-rocks-stack.ts
+++ cdk/infrastructure/lib/fanderl-rocks-stack.ts
@@ -2,10 +2,14 @@ import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as certificatemanager from 'aws-cdk-lib/aws-certificatemanager'
+import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch'
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
+import * as iam from 'aws-cdk-lib/aws-iam'
 import * as route53 from 'aws-cdk-lib/aws-route53'
 import * as targets from 'aws-cdk-lib/aws-route53-targets'
 import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment'
-import { TargetTrackingScalingPolicy } from 'aws-cdk-lib/aws-applicationautoscaling';
+import { Certificate } from 'crypto';
 
 export class FanderlRocksStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -13,40 +17,109 @@ export class FanderlRocksStack extends Stack {
 
     const subdomain = 'www';
     const domainName = 'fanderl.rocks';
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, 'cloudfront-OAI', {
+      comment: `OAI for ${this.stackName}`
+    });
 
     const root_bucket = new s3.Bucket(this, 'fanderl_rocks_domain_root_bucket', {
       versioned: false,
       bucketName: domainName,
       websiteIndexDocument: 'index.html',
-      publicReadAccess: true,
-      removalPolicy: RemovalPolicy.DESTROY
+      removalPolicy: RemovalPolicy.DESTROY,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL
     });
 
+    root_bucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [root_bucket.arnForObjects('*')],
+      principals: [new iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+    }));
+
+    const wwwFullDomainName = `${subdomain}.${domainName}`;
     const subdomain_bucket = new s3.Bucket(this, 'fanderl_rocks_subdomain_bucket', {
       versioned: false,
-      bucketName: `${subdomain}.${domainName}`,
-      websiteRedirect: {
-        hostName: domainName
-      },
-      removalPolicy: RemovalPolicy.DESTROY
+      bucketName: wwwFullDomainName,
+      // websiteRedirect: {
+      // hostName: domainName
+      // },
+      removalPolicy: RemovalPolicy.DESTROY,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      websiteIndexDocument: 'index.html'
     });
+
+    subdomain_bucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [subdomain_bucket.arnForObjects('*')],
+      principals: [new iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+    }));
 
     new s3Deployment.BucketDeployment(this, 'fanderl_rocks_static_website_content', {
       sources: [s3Deployment.Source.asset('../../content')],
       destinationBucket: root_bucket
     });
 
-    const hosted_zone = route53.HostedZone.fromLookup(this, 'fanderl_rocks_hosted_zone', {domainName});
+    new s3Deployment.BucketDeployment(this, 'fanderl_rocks_static_website_subdomain_content', {
+      sources: [s3Deployment.Source.asset('../../content')],
+      destinationBucket: subdomain_bucket
+    });
 
-    new route53.ARecord(this, 'fanderl_rocks_root_a_record', {
-      zone: hosted_zone,
-      target: route53.RecordTarget.fromAlias(new targets.BucketWebsiteTarget(root_bucket))
-    });
-    
+    const hostedZone = route53.HostedZone.fromLookup(this, 'fanderl_rocks_hosted_zone', { domainName });
+
+    const certificate = new certificatemanager.DnsValidatedCertificate(
+      this,
+      "fanderl_rocks_tls_certificate",
+      {
+        domainName: wwwFullDomainName,
+        hostedZone: hostedZone,
+        region: "us-east-1"
+      }
+    )
+
+    const viewerCertificate = cloudfront.ViewerCertificate.fromAcmCertificate({
+      certificateArn: certificate.certificateArn,
+      env: {
+        region: "eu-west-1",
+        account: "***REMOVED***"
+      },
+      node: this.node,
+      stack: this,
+      metricDaysToExpiry: () =>
+        new cloudwatch.Metric({
+          namespace: "TLS Viewer Certificate Validity",
+          metricName: "TLS Viewer Certificate Expired",
+        }),
+      applyRemovalPolicy: () => RemovalPolicy.DESTROY
+    }, {
+      sslMethod: cloudfront.SSLMethod.SNI,
+      securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+      aliases: [wwwFullDomainName]
+    })
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'fanderlf_rocks_cloudfront_distribution', {
+      viewerCertificate,
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: subdomain_bucket,
+            originAccessIdentity: cloudfrontOAI
+          },
+          behaviors: [{
+            compress: true,
+            allowedMethods: cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+            isDefaultBehavior: true,
+            viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
+          }]
+        }
+      ]
+    })
+
     new route53.ARecord(this, 'fanderl_rocks_subdmain_a_record', {
-      zone: hosted_zone,
-      recordName: subdomain,
-      target: route53.RecordTarget.fromAlias(new targets.BucketWebsiteTarget(subdomain_bucket))
+      zone: hostedZone,
+      recordName: wwwFullDomainName,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution))
     });
+
   }
 }
